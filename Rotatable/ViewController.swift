4ef7066afc976273d8b6e09f978199ff885aeac8
Simon Gladman <sgladman@gmail.com>
2015-10-01 19:21:25 +0100

    Initial Commit

--- Rotatable/ViewController.swift
+++ Rotatable/ViewController.swift
@@ -10,16 +10,121 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var label = UILabel()
+    var switchControl = UISwitch()
+    var datePicker = UIDatePicker()
+    var segmentedControl = UISegmentedControl(items: ["AAA", "BBB", "CCC", "DDD"])
+    
+    let toolbarContainer = UIStackView()
+    
+    let resetButton = UIButton(type: UIButtonType.System)
+    let slider = UISlider()
+    let infoLabel = UILabel()
+    let blur = UIVisualEffectView(effect: UIBlurEffect())
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Rotatable Label"
+        
+        view.addSubview(label)
+        view.addSubview(switchControl)
+        view.addSubview(segmentedControl)
+        view.addSubview(datePicker)
+
+        view.addSubview(blur)
+        view.addSubview(toolbarContainer)
+        
+        // ----
+        
+        slider.minimumValue = -180
+        slider.maximumValue = 180
+        
+        slider.frame = CGRect(x: 0, y: view.frame.height - slider.intrinsicContentSize().height, width: view.frame.width, height: slider.intrinsicContentSize().height)
+        
+        toolbarContainer.addArrangedSubview(slider)
+        
+        slider.addTarget(self, action: "sliderChangeHandler", forControlEvents: UIControlEvents.ValueChanged)
+        
+        // -----
+        
+        view.addSubview(toolbarContainer)
+        
+        resetButton.setTitle("Reset", forState: UIControlState.Normal)
+        
+        resetButton.tintColor = UIColor.blueColor()
+        
+        resetButton.frame = CGRect(x: 20, y: view.frame.height - 100, width: resetButton.intrinsicContentSize().width, height: resetButton.intrinsicContentSize().width)
+        
+        resetButton.addTarget(self, action: "resetRotation", forControlEvents: UIControlEvents.TouchDown)
+        
+        toolbarContainer.addArrangedSubview(resetButton)
+        
+        // ----
+        
+        toolbarContainer.addArrangedSubview(infoLabel)
+        
+        updateInfoLabel()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func resetRotation()
+    {
+        label.rotate(radians: 0, animated: true)
+        datePicker.rotate(radians: 0, animated: true)
+        switchControl.rotate(radians: 0, animated: true)
+        segmentedControl.rotate(radians: 0, animated: true)
+        
+        slider.value = 0
+        
+        updateInfoLabel()
     }
+    
+    func sliderChangeHandler()
+    {
+        label.rotate(degrees: CGFloat(slider.value))
+        datePicker.rotate(degrees: CGFloat(slider.value))
+        switchControl.rotate(degrees: CGFloat(slider.value))
+        segmentedControl.rotate(degrees: CGFloat(slider.value))
+        
+        updateInfoLabel()
+    }
+    
+    func updateInfoLabel()
+    {
+        infoLabel.text = "\(Int(label.rotation.degrees))°"
+    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        label.frame = CGRect(origin: CGPoint(x: view.frame.width * 0.25 - label.intrinsicContentSize().width * 0.5,
+            y: view.frame.height * 0.25),
+            size: CGSize(width: label.intrinsicContentSize().width,
+                height: label.intrinsicContentSize().height))
 
+        switchControl.frame = CGRect(origin: CGPoint(x: view.frame.width * 0.25 - switchControl.intrinsicContentSize().width * 0.5,
+            y: view.frame.height * 0.5),
+            size: CGSize(width: switchControl.intrinsicContentSize().width, height: switchControl.intrinsicContentSize().height))
 
+        segmentedControl.frame = CGRect(origin: CGPoint(x: view.frame.width * 0.25 - segmentedControl.intrinsicContentSize().width * 0.5,
+            y: view.frame.height * 0.75),
+            size: CGSize(width: segmentedControl.intrinsicContentSize().width, height: segmentedControl.intrinsicContentSize().height))
+        
+        datePicker.frame = CGRect(origin: CGPoint(x: view.frame.width * 0.66 - datePicker.intrinsicContentSize().width * 0.5,
+            y: view.frame.height * 0.5 - datePicker.intrinsicContentSize().height * 0.5),
+            size: CGSize(width: datePicker.intrinsicContentSize().width, height: datePicker.intrinsicContentSize().height))
+        
+        toolbarContainer.frame = CGRect(x: 5,
+            y: view.frame.height - 50,
+            width: view.frame.width - 10,
+            height: 50)
+        
+        blur.frame = toolbarContainer.frame
+        
+        toolbarContainer.spacing = 10
+    }
 }
+
+
 
---
+++ Rotatable/rotatable/Rotatable.swift
@@ -0,0 +1,64 @@
+//
+//  Rotatable.swift
+//  Rotatable
+//
+//  Created by Simon Gladman on 01/10/2015.
+//  Copyright © 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+protocol Rotatable
+{
+    var transform: CGAffineTransform {get set}
+    
+    mutating func rotate(degrees degrees: CGFloat, animated: Bool)
+    mutating func rotate(radians radians: CGFloat, animated: Bool)
+    
+    var rotation: (radians: CGFloat, degrees: CGFloat) { get }
+}
+
+extension UIView: Rotatable
+{
+    
+}
+
+extension Rotatable
+{
+    mutating func rotate(degrees degrees: CGFloat, animated: Bool = false)
+    {
+        rotate(radians: degreesToRadians(degrees), animated: animated)
+    }
+    
+    mutating func rotate(radians radians: CGFloat, animated: Bool = false)
+    {
+        if animated
+        {
+            UIView.animateWithDuration(0.2)
+            {
+                self.transform = CGAffineTransformMakeRotation(radians)
+            }
+        }
+        else
+        {
+            transform = CGAffineTransformMakeRotation(radians)
+        }
+    }
+    
+    var rotation: (radians: CGFloat, degrees: CGFloat)
+    {
+        let radians = CGFloat(atan2f(Float(transform.b), Float(transform.a)))
+        
+        return (radians, radiansToDegrees(radians))
+    }
+    
+    func degreesToRadians(value: CGFloat) -> CGFloat
+    {
+        return CGFloat(M_PI) * value / 180.0
+    }
+    
+    func radiansToDegrees(value: CGFloat) -> CGFloat
+    {
+        return value * 180 / CGFloat(M_PI)
+    }
+}
